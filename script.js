45ce4b8cab38287152efca14107ba69f3fa44043
mzcz <mzcz@wp.pl>
2017-12-30 15:19:58 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://https://afternoon-falls-74366.herokuapp.com/v1/task/';
+  var apiRoot = 'https://afternoon-falls-74366.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
