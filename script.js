0e7d46c4994959b5e59d187dbfe939ecf27cfaf9
mzcz <mzcz@wp.pl>
2018-02-18 00:09:49 +0100

    heroku

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://fternoon-falls-74366.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://fternoon-falls-74366.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://fternoon-falls-74366.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://fternoon-falls-74366.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
